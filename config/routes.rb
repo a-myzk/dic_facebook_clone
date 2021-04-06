80367499ee7d671e4d5d24bb1bdf22e157d580cc
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-04-06 15:34:10 +0900

    ログインシステム実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
   get '/pictures', to: 'pictures#index'
   resources :pictures do
     collection do
