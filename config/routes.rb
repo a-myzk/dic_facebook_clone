c6ac55f4046fabf2c484e86d74abe2191301adb2
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-04-07 11:19:29 +0900

    config/route.rb-change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  get '/pictures', to: 'pictures#index'
+  root '/pictures', to: 'pictures#index'
   resources :pictures do
     collection do
       post :confirm
